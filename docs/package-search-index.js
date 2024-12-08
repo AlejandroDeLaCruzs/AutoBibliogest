75068523877a007df60c9a8b56c8e998d232c521
alejandrodelacruz <alexcruzs1105@gmail.com>
2024-12-08 23:46:22 +0100

    v11

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Core"},{"l":"GUI"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"App"},{"l":"Core"},{"l":"Core.Notificaciones"},{"l":"GUI"}];updateSearchResults();
